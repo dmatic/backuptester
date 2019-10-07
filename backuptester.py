9c6e94f847c3ac0d254f6623570c0fd0e38e9e0c
Dragan Matic <dragan@draganmatic.com>
2019-10-07 13:45:08 +0200

    added previous status check -

--- backuptester.py
+++ backuptester.py
@@ -87,6 +87,20 @@ def start():
     directories = []
 
     errors = []
+    PreviousCheckOk = True
+
+
+    try:
+        f = open(config["global"]["statfile"], "rt")
+        lines = f.readlines()
+        for line in lines:            
+            values = json.loads(line)
+            if 'globalstatus' in values and values['globalstatus'] == 'error':
+                PreviousCheckOk = False                        
+    except Exception as e:
+        print(e)
+        pass
+
 
     for directory in config["dirs"]:
         if len(config["dirs"][directory].splitlines()) > 1:
@@ -99,52 +113,58 @@ def start():
     for directory, values in directories:
         if isinstance(values, list):
             for value in values:
+                if not silent:            
+                    print("Checking directory {0}, value {1}".format(directory, value))
                 e = DoCheck(directory, value, f)
                 if e:
                 	errors.append(e)
         else:
+            if not silent:            
+                print("Checking directory {0}, value {1}".format(directory, values))
             e = DoCheck(directory, values, f)
             if e:
             	errors.append(e)
 
-
     if errors:
-        # send email
+        if PreviousCheckOk:
+            f.write("{0}\n".format(json.dumps({"globalstatus":"error"})))
 
-        _DEFAULT_CIPHERS = ('ECDH+AESGCM:DH+AESGCM:ECDH+AES256:DH+AES256:ECDH+AES128:DH+AES:ECDH+HIGH:DH+HIGH:ECDH+3DES:DH+3DES:RSA+AESGCM:RSA+AES:RSA+HIGH:RSA+3DES:!aNULL:!eNULL:!MD5')
-        smtp_server = smtplib.SMTP(config['global']['smtp'], port=str(config['global']['smtp_port']))
+            # send email
 
-        # only TLSv1 or higher
-        context = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
-        context.options |= ssl.OP_NO_SSLv2
-        context.options |= ssl.OP_NO_SSLv3
+            _DEFAULT_CIPHERS = ('ECDH+AESGCM:DH+AESGCM:ECDH+AES256:DH+AES256:ECDH+AES128:DH+AES:ECDH+HIGH:DH+HIGH:ECDH+3DES:DH+3DES:RSA+AESGCM:RSA+AES:RSA+HIGH:RSA+3DES:!aNULL:!eNULL:!MD5')
+            smtp_server = smtplib.SMTP(config['global']['smtp'], port=str(config['global']['smtp_port']))
 
-        context.set_ciphers(_DEFAULT_CIPHERS)
-        context.set_default_verify_paths()
-        context.verify_mode = ssl.CERT_REQUIRED
+            # only TLSv1 or higher
+            context = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
+            context.options |= ssl.OP_NO_SSLv2
+            context.options |= ssl.OP_NO_SSLv3
 
-        if smtp_server.starttls()[0] != 220:
-            return False # cancel if connection is not encrypted
-        smtp_server.login(config['global']['smtp_login'], config['global']['smtp_password'])
+            context.set_ciphers(_DEFAULT_CIPHERS)
+            context.set_default_verify_paths()
+            context.verify_mode = ssl.CERT_REQUIRED
+
+            if smtp_server.starttls()[0] != 220:
+                return False # cancel if connection is not encrypted
+            smtp_server.login(config['global']['smtp_login'], config['global']['smtp_password'])
 
 
-        BODY = "\r\n".join((
-            "From: %s" % config['global']['from'],
-            "To: %s" % config['global']['to'],
-            "Subject: %s" % "backup errors" ,
-            "",
-            str(errors)
-            ))
+            BODY = "\r\n".join((
+                "From: %s" % config['global']['from'],
+                "To: %s" % config['global']['to'],
+                "Subject: %s" % "backup errors" ,
+                "",
+                str(errors)
+                ))
 
-        smtp_server.sendmail(config['global']['from'],config['global']['to'], BODY)
+            smtp_server.sendmail(config['global']['from'],config['global']['to'], BODY)
 
-        # send sms
+            # send sms
 
-        if sendsms:
-            sendSMS(config['sms']['plan_id'], 
-                config['sms']['api_token'], 
-                config['sms']['number_from'], 
-                config['sms']['number_to'], ['sms']['message'])
+            if sendsms:
+                sendSMS(config['sms']['plan_id'], 
+                    config['sms']['api_token'], 
+                    config['sms']['number_from'], 
+                    config['sms']['number_to'], config['sms']['message'])
 
         if not silent:            
             print("❌ There were errors")
@@ -152,9 +172,10 @@ def start():
                 print(error)
 
     else:
+
+        f.write("{0}\n".format(json.dumps({"globalstatus:":"ok"})))
         if not silent:            
             print("✅ Everything ok")
-
 
     f.close()
 
